7720aeb709080aa096b2a92f23315e0354eb9585
Alice <alice.storey91@gmail.com>
2022-10-14 08:33:35 -0400

    transaction service: update

--- src/main/java/com/alice/RewardsProgram/model/Transaction.java
+++ src/main/java/com/alice/RewardsProgram/model/Transaction.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -12,11 +13,17 @@ import java.util.Optional;
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class Transaction {
     private long transactionId, userId;
     private List<Item> cartItems;
     private Date date;
+
+    final private double ONE_POINT_MINIMUM = 50.0;
+    final private double TWO_POINT_MINIMUM = 100.00;
+
+    public Transaction() {
+        cartItems = new ArrayList<>();
+    }
 
     public double getTotalPrice() {
         if (this.cartItems == null)
@@ -37,5 +44,18 @@ public class Transaction {
 
     public void removeItem(long itemId) {
         this.cartItems.removeIf(item->item.itemId==itemId);
+    }
+
+    public long getRewardPoints() {
+        long rewardsPts = 0L;
+        double price = this.getTotalPrice();
+        if (price > TWO_POINT_MINIMUM) {
+            rewardsPts += (price - TWO_POINT_MINIMUM) * 2;
+            price = TWO_POINT_MINIMUM;
+        }
+        if (price > ONE_POINT_MINIMUM)
+            rewardsPts += (price - ONE_POINT_MINIMUM);
+
+        return rewardsPts;
     }
 }
--- src/test/java/com/alice/RewardsProgram/model/TransactionTest.java
+++ src/test/java/com/alice/RewardsProgram/model/TransactionTest.java
@@ -52,4 +52,26 @@ public class TransactionTest {
         t.removeItem(item2.getItemId());
         assertEquals(0.0, t.getTotalPrice());
     }
+
+    @Test
+    public void testRewardsPoints() {
+        Transaction t = new Transaction();
+        Item item1 = new Item(), item2 = new Item();
+        double p1 = 50.0, p2 = 90.0;
+        item1.setItemName("Broom");
+        item1.setItemId(1);
+        item1.setItemPrice(p1);
+        item2.setItemPrice(p2);
+        item2.setItemId(2);
+        item2.setItemName("Gum");
+
+        assertEquals(0L, t.getRewardPoints());
+        t.addItem(item1);
+        assertEquals(0L, t.getRewardPoints());
+        t.removeItem(item1);
+        t.addItem(item2);
+        assertEquals(40L, t.getRewardPoints());
+        t.addItem(item1);
+        assertEquals(80L, t.getRewardPoints());
+    }
 }
